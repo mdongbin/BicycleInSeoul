611d479094c0fff2ae5a0fdcc137f96dc98e9752
mdongbin <rewgh0@gmail.com>
2020-09-07 23:49:16 +0900

    알람기능 추가.

--- app/src/main/java/com/example/bicycleinseoul/AlarmFragment.java
+++ app/src/main/java/com/example/bicycleinseoul/AlarmFragment.java
@@ -1,6 +1,7 @@
 package com.example.bicycleinseoul;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,11 +10,35 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import ir.samanjafari.easycountdowntimer.CountDownInterface;
+import ir.samanjafari.easycountdowntimer.EasyCountDownTextview;
+
 public class AlarmFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_alarm, container, false);
+        EasyCountDownTextview easyCountDownTextview = (EasyCountDownTextview) view.findViewById(R.id.easyCountDownTextview);
+
+        easyCountDownTextview.setTime(1,1, 30, 15);// setTime(days, hours, minute, second)
+        easyCountDownTextview.setTextSize(30);
+        easyCountDownTextview.startTimer();
+//        EasyCountDownTextview countDownTextview = (EasyCountDownTextview) view.findViewById(R.id.easyCountDownTextview);
+//        easyCountDownTextview.setTime(1, 1, 1, 1);
+
+        easyCountDownTextview.setOnTick(new CountDownInterface() {
+            @Override
+            public void onTick(long time) {
+                Log.e("#", "tick!");
+            }
+
+            @Override
+            public void onFinish() {
+                Log.e("@", "finish!");
+
+            }
+        });
+
         return view;
     }
 }
